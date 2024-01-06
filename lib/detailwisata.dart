92cedfabe7fd0f9f423f5df754ed6c328a2aa992
naufalrusada <nopalr123@gmail.com>
2024-01-06 23:22:54 +0700

    Menampilkan Daftar Wisata

--- lib/detailwisata.dart
+++ lib/detailwisata.dart
@@ -3,86 +3,88 @@ import 'package:flutter_pemula/model/tourism_place.dart';
 
 class DetailWisata extends StatelessWidget {
   final TourismPlace place;
+
   const DetailWisata({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
         body: SafeArea(
-          child: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          IconButton(icon: const Icon(Icons.arrow_back), onPressed: () {
-            Navigator.pop(context);
-          }),
-          Image.asset(place.imageAsset),
-          Container(
-            margin: EdgeInsets.only(top: 16.0),
-            alignment: Alignment.center,
-            child: const Text(
-              "Wisata MEnara Kudus",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Stack(
               children: <Widget>[
-                Column(children: <Widget>[
-                  Icon(Icons.calendar_month),
-                  Text("")
-                ]
+                IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                Image.asset(place.imageAsset),
+                Container(
+                  margin: EdgeInsets.only(top: 16.0),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Wisata MEnara Kudus",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
                 ),
-
-
-                Column(children: <Widget>[
-                  Icon(Icons.lock_clock),
-                  Text("Open Everyday")
-                ]
+                Container(
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(children: <Widget>[
+                        Icon(Icons.calendar_month),
+                        Text("")
+                      ]),
+                      Column(children: <Widget>[
+                        Icon(Icons.lock_clock),
+                        Text("Open Everyday")
+                      ]),
+                      Column(children: <Widget>[
+                        Icon(Icons.monetization_on),
+                        Text("Open Everyday")
+                      ]),
+                    ],
+                  ),
                 ),
-
-                Column(children: <Widget>[
-                  Icon(Icons.monetization_on),
-                  Text("Open Everyday")
-                ]
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable",
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            child: Text(
-              "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable",
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          SizedBox(
-            height: 150,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
-          ),
     ));
   }
 }
